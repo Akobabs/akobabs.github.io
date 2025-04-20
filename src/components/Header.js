795d725a1491950a7776f52b1cd16852aa221163
Akorede A., ADEMOLA <77745824+Akobabs@users.noreply.github.com>
2025-04-20 08:14:39 +0100

    Added Hamburger menu
    For better mobile adaptability

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,31 @@
-// src/components/Header.js
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <header>
             <nav>
                 <div className="nav-container">
                     <Link to="/" className="site-title">ADEMOLA, Akorede Adejare</Link>
-                    <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/cv">CV</Link></li>
-                        <li><Link to="/education">Education</Link></li>
-                        <li><Link to="/projects">Projects</Link></li>
-                        <li><Link to="/skills">Skills</Link></li>
-                        <li><Link to="/publications">Publications</Link></li>
-                        <li><Link to="/contact">Contact</Link></li>
+                    {/* Hamburger Icon */}
+                    <button className="hamburger" onClick={toggleMenu}>
+                        <i className={isMenuOpen ? 'fas fa-times' : 'fas fa-bars'}></i>
+                    </button>
+                    {/* Navigation Links */}
+                    <ul className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
+                        <li><Link to="/" onClick={() => setIsMenuOpen(false)}>Home</Link></li>
+                        <li><Link to="/cv" onClick={() => setIsMenuOpen(false)}>CV</Link></li>
+                        <li><Link to="/education" onClick={() => setIsMenuOpen(false)}>Education</Link></li>
+                        <li><Link to="/projects" onClick={() => setIsMenuOpen(false)}>Projects</Link></li>
+                        <li><Link to="/skills" onClick={() => setIsMenuOpen(false)}>Skills</Link></li>
+                        <li><Link to="/publications" onClick={() => setIsMenuOpen(false)}>Publications</Link></li>
+                        <li><Link to="/contact" onClick={() => setIsMenuOpen(false)}>Contact</Link></li>
                     </ul>
                 </div>
             </nav>
